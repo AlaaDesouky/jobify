a8420aac9bc83c1e625c705f0b567438937d0165
Desouky <alaa.desouky33@gmail.com>
2022-04-04 14:58:07 +0200

    Shared layout structure

--- src/pages/dashboard/SharedLayout.js
+++ src/pages/dashboard/SharedLayout.js
@@ -1,9 +1,22 @@
-
+import { Outlet } from 'react-router-dom'
+import Wrapper from '../../assets/wrappers/SharedLayout'
+import { BigSidebar, Navbar, SmallSidebar } from '../../components'
+import { useAppContext } from '../../context/appContext'
 const SharedLayout = () => {
+  const { user } = useAppContext()
   return (
-    <div>
-      Shared Layout
-    </div>
+    <Wrapper>
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
   )
 }
 

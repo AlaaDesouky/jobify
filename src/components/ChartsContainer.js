a4033eddf0ea8f93ec2e9ed81f5600db308cf407
Desouky <alaa.desouky33@gmail.com>
2022-04-08 17:52:36 +0200

    Handle Stats chart

--- src/components/ChartsContainer.js
+++ src/components/ChartsContainer.js
@@ -1,9 +1,23 @@
+import { useState } from 'react'
+import BarChart from './BarChart'
+import AreaChart from './AreaChart'
+import { useAppContext } from '../context/appContext'
+import Wrapper from '../assets/wrappers/ChartsContainer'
 
 const ChartsContainer = () => {
+  const [isBarChart, setIsBarChart] = useState(true)
+  const { monthlyApplications: data } = useAppContext()
+
+  console.log(data)
+
   return (
-    <div>
-      charts container
-    </div>
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type='button' onClick={() => setIsBarChart(!isBarChart)}>
+        {isBarChart ? 'AreaChart' : 'BarChart'}
+      </button>
+      {isBarChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
   )
 }
 
--- src/context/appContext.js
+++ src/context/appContext.js
@@ -270,7 +270,6 @@ const AppProvider = ({ children }) => {
     dispatch({ type: SHOW_STATS_BEGIN })
     try {
       const { data } = await authFetch('/jobs/stats')
-      console.log(data)
       dispatch({
         type: SHOW_STATS_SUCCESS,
         payload: {
